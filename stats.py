39fe435e7fb4bf534e1cf6bfbe546d3fc2b54730
Shuabble <127648888+Shuabble@users.noreply.github.com>
2023-03-12 15:32:23 +0800

    Mean Function

--- stats.py
+++ stats.py
@@ -8,3 +8,12 @@ def mode(numbers):
     max_count = max(count_dict.values())
     mode_list = [k for k, v in count_dict.items() if v == max_count]
     return mode_list[0]
+
+# Mean function, length of number list will be determined using python's inbuilt length function,
+# and the sum of all numbers will be determined using python's inbuilt sum function.
+# Function will return calculated mean value.
+def meanCalculation(numbers):
+    listLength = len(numbers)
+    summation = sum(numbers)
+    mean = summation/listLength
+    return mean
