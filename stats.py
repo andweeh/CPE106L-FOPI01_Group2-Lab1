c0c80f71296d5f7ec24dc589eeafd29925569465
RaphaelLight <115327472+RaphaelLight@users.noreply.github.com>
2023-03-12 23:10:52 +0800

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,12 +1,3 @@
-# Mean function, length of number list will be determined using python's inbuilt length function,
-# and the sum of all numbers will be determined using python's inbuilt sum function.
-# Function will return calculated mean value.
-def mean(numbers):
-    listLength = len(numbers)
-    summation = sum(numbers)
-    mean = summation/listLength
-    return mean
-
 def median(list):
     """
     Author: Liwanag, John Rafael L.
@@ -19,14 +10,3 @@ def median(list):
     else:
         median = list[int(n/2)]
     return median
-
-def mode(numbers):
-    count_dict = {}
-    for num in numbers:
-        if num in count_dict:
-            count_dict[num] += 1
-        else:
-            count_dict[num] = 1
-    max_count = max(count_dict.values())
-    mode_list = [k for k, v in count_dict.items() if v == max_count]
-    return mode_list
