48ef34191a8a7115a4709e6e49526b44d8b4d09d
Paul Jess Rosero <125846655+pauljessrosero@users.noreply.github.com>
2023-03-12 21:30:54 +0800

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,13 +1,23 @@
-def mode(numbers):
-    count_dict = {}
-    for num in numbers:
-        if num in count_dict:
-            count_dict[num] += 1
+#Paul Jess C. Rosero
+#PreLab - 1 Contribution
+#Programming Problem 1 - Mode Function
+from collections import Counter
+
+def modeCalculation(numbers):
+    frequency = Counter(numbers)
+    modes = frequency.most_common()
+    max_count = modes[0][1]
+    mode_list = []
+    for mode in modes:
+        if mode[1] == max_count:
+            mode_list.append(mode[0])
         else:
-            count_dict[num] = 1
-    max_count = max(count_dict.values())
-    mode_list = [k for k, v in count_dict.items() if v == max_count]
-    return mode_list[0]
+            break
+    return min(mode_list)
+number_string = input("Enter a list of numbers separated by commas: ")
+number_list = [int(num) for num in number_string.split(',')]
+mode = modeCalculation(number_list)
+print("The mode of the input list is: ", mode)
 
 # Mean function, length of number list will be determined using python's inbuilt length function,
 # and the sum of all numbers will be determined using python's inbuilt sum function.
