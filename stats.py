5256e6610b2ed3747ca6ef7699bdf6bb8ef3b214
RaphaelLight <115327472+RaphaelLight@users.noreply.github.com>
2023-03-12 22:49:06 +0800

    Update stats.py
    All these functions have been tested and work properly.

--- stats.py
+++ stats.py
@@ -1,29 +1,32 @@
-#Paul Jess C. Rosero
-#PreLab - 1 Contribution
-#Programming Problem 1 - Mode Function
-from collections import Counter
-
-def modeCalculation(numbers):
-    frequency = Counter(numbers)
-    modes = frequency.most_common()
-    max_count = modes[0][1]
-    mode_list = []
-    for mode in modes:
-        if mode[1] == max_count:
-            mode_list.append(mode[0])
-        else:
-            break
-    return min(mode_list)
-number_string = input("Enter a list of numbers separated by commas: ")
-number_list = [int(num) for num in number_string.split(',')]
-mode = modeCalculation(number_list)
-print("The mode of the input list is: ", mode)
-
 # Mean function, length of number list will be determined using python's inbuilt length function,
 # and the sum of all numbers will be determined using python's inbuilt sum function.
 # Function will return calculated mean value.
-def meanCalculation(numbers):
+def mean(numbers):
     listLength = len(numbers)
     summation = sum(numbers)
     mean = summation/listLength
     return mean
+
+def median(list):
+    """
+    Author: Liwanag, John Rafael L.
+    Takes as a sorted list of numbers as an argument 
+    and returns the median.
+    """
+    n = len(list)
+    if(n % 2 == 0):
+        median = (list[int(n/2)] + list[int((n/2)-1)])/2
+    else:
+        median = list[int(n/2)]
+    return median
+
+def mode(numbers):
+    count_dict = {}
+    for num in numbers:
+        if num in count_dict:
+            count_dict[num] += 1
+        else:
+            count_dict[num] = 1
+    max_count = max(count_dict.values())
+    mode_list = [k for k, v in count_dict.items() if v == max_count]
+    return mode_list
